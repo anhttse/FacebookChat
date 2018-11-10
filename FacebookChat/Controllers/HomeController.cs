b520dadd973bb6e9803a0d9c242eaf840ffac881
anhttse <zlegendzz@gmail.com>
2018-11-10 17:03:56 +0700

    update

--- FacebookChat/Controllers/HomeController.cs
+++ FacebookChat/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 
 namespace FacebookChat.Controllers
 {
@@ -11,6 +12,11 @@ namespace FacebookChat.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public void Webhook(object evt)
+        {
+            System.IO.File.AppendAllText(Server.MapPath("~/Content/webhook.txt"),JsonConvert.SerializeObject(evt));
         }
 
         public ActionResult About()
